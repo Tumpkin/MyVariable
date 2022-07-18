c3374838de6af2ad85690c724f73eda991f564ec
Jisu Hwang <ghkdwltn5@naver.com>
2022-07-18 13:07:06 +0900

    MyVariable

---
+++ src/variableTest/Variable.java
@@ -0,0 +1,17 @@
+package variableTest;
+
+public class Variable {
+	public static void main(String[] args) {
+		int age = 10;
+		float interrestRate = 3.55678F;
+		double compoundInterestRate = 3.55678;
+		char k_cow_grade = 'A';
+		String data = "ABC";
+		
+		System.out.println(age);
+		System.out.println(interrestRate);
+		System.out.println(compoundInterestRate);
+		System.out.println(k_cow_grade);
+		System.out.println(data);
+	}
+}
